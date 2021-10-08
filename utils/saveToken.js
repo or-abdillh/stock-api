f1353ab070dafac3c9b1f90d97ec275ce3a0e673
or-abdillh <hans.abdillh05@gmail.com>
2021-10-08 19:49:32 +0800

    Save the time token has create

--- utils/saveToken.js
+++ utils/saveToken.js
@@ -1,8 +1,12 @@
 'use strict'
 
 //Insert TOKEN to table User
+//Save the time token has create
 const saveToken = (conn, TOKEN, username) => {
-   const sql = `UPDATE User SET token = "${TOKEN}" 
+   //Get the time
+   const createAt = new Date().getTime();
+   //Create query
+   const sql = `UPDATE User SET token = "${TOKEN}", token_create_at = "${createAt}" 
       WHERE username = "${username}"`;
       
    conn.query(sql, (err, rows, fields) => {
