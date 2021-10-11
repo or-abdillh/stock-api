2f2c721bf0a1d58fbb4978fe12bf2dc1f5d34d32
or-abdillh <hans.abdillh05@gmail.com>
2021-10-11 21:46:13 +0800

    Generate name file after upload success

--- controller/modules/upload.js
+++ controller/modules/upload.js
@@ -3,7 +3,6 @@ var response = require('../../response/response.js')
 
 const upload = (req, res) => {
    
-   console.log(req)
    //If file not exist
    if (!req.files) {
       response.serverError('File not found', res);
@@ -11,15 +10,22 @@ const upload = (req, res) => {
    // accessing the file
    const myFile = req.files.file;
    
-    //  mv() method places the file inside public directory
-    myFile.mv(`public/${myFile.name}`, err => {
+   //Get extension
+   let ext = myFile.name.split('.');
+   ext = ext[ext.length - 1];
+   
+   //Generate name file
+   const nameFile = `product-${new Date().getTime()}.${ext}`;
+
+   //  mv() method places the file inside public directory
+   myFile.mv(`public/${nameFile}`, err => {
         if (err) {
             console.log(err)
             response.serverError('Something wrong', res);
         }
         // returing the response with file path and name
-        response.success({ path: `http://${req.hostname}:8080/public/${myFile.name}` }, res);
-    });
+        response.success({ path: `http://${req.hostname}:8080/public/${nameFile}` }, res);
+   });
 }
 
 module.exports = upload;
