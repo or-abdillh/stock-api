f907be6fd7b95d8c0e4a018644dec56285ab1e88
or-abdillh <hans.abdillh05@gmail.com>
2021-10-30 22:35:48 +0800

    Update README

---
+++ DB/stockAppDB.sql
@@ -0,0 +1,110 @@
+
+-- MariaDB dump 10.19  Distrib 10.6.4-MariaDB, for Android (aarch64)
+--
+-- Host: localhost    Database: stock_app
+-- ------------------------------------------------------
+-- Server version	10.6.4-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Categorys`
+--
+
+DROP TABLE IF EXISTS `Categorys`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Categorys` (
+  `id_category` int(10) NOT NULL AUTO_INCREMENT,
+  `category` varchar(30) NOT NULL,
+  `create_at` bigint(50) NOT NULL,
+  PRIMARY KEY (`id_category`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Categorys`
+--
+
+LOCK TABLES `Categorys` WRITE;
+/*!40000 ALTER TABLE `Categorys` DISABLE KEYS */;
+INSERT INTO `Categorys` VALUES (26,'Assets',1635568313059),(29,'UI',1635583995393);
+/*!40000 ALTER TABLE `Categorys` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Products`
+--
+
+DROP TABLE IF EXISTS `Products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Products` (
+  `id_product` int(10) NOT NULL AUTO_INCREMENT,
+  `name_product` varchar(30) DEFAULT NULL,
+  `price_product` int(20) DEFAULT NULL,
+  `stock_product` int(20) DEFAULT NULL,
+  `image_product` varchar(100) DEFAULT NULL,
+  `category_product` varchar(30) DEFAULT NULL,
+  `last_modified` bigint(30) DEFAULT NULL,
+  `stock_unit` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id_product`)
+) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Products`
+--
+
+LOCK TABLES `Products` WRITE;
+/*!40000 ALTER TABLE `Products` DISABLE KEYS */;
+INSERT INTO `Products` VALUES (76,'Happy Cuan Figma UI Kits',567000,2,'http://localhost:8080/public/product-1635568922040.jpg','UI',1635585093337,'pcs');
+/*!40000 ALTER TABLE `Products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `User` (
+  `username` varchar(30) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `fullname` varchar(30) DEFAULT NULL,
+  `token` varchar(100) DEFAULT NULL,
+  `token_create_at` bigint(30) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `User`
+--
+
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+INSERT INTO `User` VALUES ('admin','2ed37ee883101500cde58c9a5d157df5','Toko Rania','ha44f3rvqc9dqyun1c50gu488fcf7nrjj492kbaixejmkdwpjr5g43gro2h4cif5gwypfgft5x50gqzp8k7bsf0yitz8awqxvlko',1635586850952);
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-10-30 21:52:52
